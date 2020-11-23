a56463119f148fa248ad5100bbba9ab106e0cfd6
LEN <LEN@LAPTOP-PA0SLDL1>
2020-11-23 23:38:06 +0530

    first commit

---
+++ Day 10/src/com/collection/RealEx.java
@@ -0,0 +1,21 @@
+package com.collection;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class RealEx 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityBlockingQueue<Integer> pbq = new PriorityBlockingQueue<Integer>();
+
+		// add numbers
+		pbq.add(1);
+		pbq.add(2);
+		pbq.add(3);
+		pbq.add(4);
+		pbq.add(5);
+
+		// print queue
+		System.out.println("PriorityBlockingQueue:" + pbq);
+	}
+}
